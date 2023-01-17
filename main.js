ac4c2f303d1cc4b4b05ab896b5e47a5b8d75b6c0
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-17 18:24:36 +0100

    finish

---
+++ main.js
@@ -0,0 +1,110 @@
+////////////                                                         EXO3                                                     ////////////
+
+// let div = document.getElementsByTagName("div");
+
+// let lastDiv = div[div.length -1];
+
+// console.log(lastDiv);
+
+// let firstElementLastDiv = lastDiv.firstChild;
+
+// console.log(firstElementLastDiv);
+
+//////////////////
+
+// let div = document.getElementsByTagName("div");
+
+// let lastDiv = div[div.length -1];
+
+// console.log(lastDiv);
+
+// let lastElementLastDiv = lastDiv.lastChild;
+
+// console.log(lastElementLastDiv);
+
+//////////////////
+
+// let div = document.getElementsByTagName("div");
+
+// console.log(div);
+
+// let lastDiv = div[div.length -1]
+
+// console.log(lastDiv);
+
+// let first = lastDiv.firstChild;
+
+// let para = first.nextElementSibling;
+
+// let i = para.children;
+
+// console.log(i);
+
+////////////////
+
+// let div = document.getElementsByTagName("div");
+
+// console.log(div);
+
+// let lastDiv = div[div.length -1]
+
+// console.log(lastDiv);
+
+// let para = lastDiv.lastChild;
+
+// console.log(para);
+
+// let b = para.children;          problem
+
+// console.log(b);
+
+/////////////////////////
+
+// let recup = document.getElementsByTagName("i")[0];
+// let parent = recup.parentNode;
+// console.log(parent);
+
+/////////////////////
+
+// let recup = document.getElementsByTagName("b")[0];
+// let parent = recup.parentNode;
+// console.log(parent);
+
+///////////////////
+
+// let allDiv = document.getElementsByTagName("div");
+// console.log(allDiv);
+
+// let lastDiv = allDiv[allDiv.length -1]
+// console.log(lastDiv.textContent);
+
+// let firstGosse = lastDiv.firstElementChild;
+// console.log(firstGosse.textContent);
+
+// let second = firstGosse.nextSibling
+// console.log(second);
+
+/////////////////// EXO 4 ///////////////////////////////
+
+// let recupH1 = document.getElementsByTagName("h1")[1];
+// console.log(recupH1.textContent);
+
+/////////////////
+
+// let recupLi = document.getElementsByTagName("li")[2];
+// console.log(recupLi.textContent);
+
+/////////////////
+
+// let para = document.getElementsByTagName('p')[0];
+
+// console.log(para.textContent.toUpperCase());
+
+//////////////////
+
+// let allLi = document.getElementsByTagName("li");
+// console.log(allLi);
+
+// Array.prototype.forEach.call(allLi, function(li) {
+//     console.log(li.textContent.toUpperCase());
+// });
